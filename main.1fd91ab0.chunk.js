f6e173415c0e2f50299ae722885d28b43b26548d
Nathan Grifiths <njgriffiths@gmail.com>
2022-06-01 11:15:47 -0700

    Updates

---
+++ main.1fd91ab0.chunk.js
@@ -0,0 +1,85 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./src/css/normalize.css
+var normalize = __webpack_require__(0);
+
+// EXTERNAL MODULE: ./src/css/postmedia.css
+var postmedia = __webpack_require__(1);
+
+// EXTERNAL MODULE: ./src/css/colors.css
+var colors = __webpack_require__(2);
+
+// EXTERNAL MODULE: ./src/css/fonts.css
+var fonts = __webpack_require__(3);
+
+// EXTERNAL MODULE: ./src/css/main.css
+var main = __webpack_require__(4);
+
+// CONCATENATED MODULE: ./src/fonts/Shift-Bold.otf
+/* harmony default export */ var Shift_Bold = (__webpack_require__.p + "assets/Shift-Bold.8c454d7e.otf");
+// CONCATENATED MODULE: ./src/fonts/Shift-BoldItalic.otf
+/* harmony default export */ var Shift_BoldItalic = (__webpack_require__.p + "assets/Shift-BoldItalic.144e2c1f.otf");
+// CONCATENATED MODULE: ./src/fonts/BentonSansCond-Regular.otf
+/* harmony default export */ var BentonSansCond_Regular = (__webpack_require__.p + "assets/BentonSansCond-Regular.4421f875.otf");
+// CONCATENATED MODULE: ./src/fonts/BentonSansCond-RegItalic.otf
+/* harmony default export */ var BentonSansCond_RegItalic = (__webpack_require__.p + "assets/BentonSansCond-RegItalic.06edc58b.otf");
+// CONCATENATED MODULE: ./src/fonts/BentonSansCond-Bold.otf
+/* harmony default export */ var BentonSansCond_Bold = (__webpack_require__.p + "assets/BentonSansCond-Bold.87a66dcd.otf");
+// CONCATENATED MODULE: ./src/fonts/BentonSansCond-BoldItalic.otf
+/* harmony default export */ var BentonSansCond_BoldItalic = (__webpack_require__.p + "assets/BentonSansCond-BoldItalic.539670da.otf");
+// CONCATENATED MODULE: ./src/index.js
+// CSS
+
+
+
+
+ // FONTS
+
+
+
+
+
+
+ // JS
+// const init = async () => {
+// };
+// init();
+
+/***/ })
+],[[5,1]]]);
